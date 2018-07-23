325272442236c68b38b5f7b072dea8ac621098b5
vargarturo344@gmail.com <vargarturo344@gmail.com>
2018-07-23 17:04:56 +0100

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangogirls',
-        'USER': 'Arturo',
-        'PASSWORD': '',
+        'USER': 'arturo',
+        'PASSWORD': 'cake123!',
         'HOST': 'localhost',
         'PORT': '',
     }
